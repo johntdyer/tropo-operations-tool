ed47eaaff10ebb1861e56ca2880927724cb5bf1f
John Dyer <jdyer@tropo.com>
2014-07-06 23:06:25 -0400

    Large refactor of library

--- commands.go
+++ commands.go
@@ -5,14 +5,63 @@ import (
 )
 
 var Commands = []cli.Command{
-	commandAddressLookup,
-  commandUserLookup,
-  commandApplicationLookup,
-  sessionIdLookup,
+  guid,
   list,
+  lookup,
 }
 
-var sessionIdLookup = cli.Command{
+func assert(err error) {
+  if err != nil {
+    logger.Fatal(err)
+  }
+}
+
+// tropo lookup address 4075551212 --user --application
+// tropo lookup address 99999999   --user --application
+// tropo lookup address abc123     --user --application
+// tropo lookup appplication 1234  --addresses
+
+// tropo guid abc123
+// tropo check dns 4075551212
+
+// tropo lookup user abc123
+// tropo lookup user abc123 --addresses
+// tropo lookup user abc123 --applications --addresses
+
+
+var lookup = cli.Command{
+  Name:  "lookup",
+  Usage: "tropo lookup 9fb9f0887171a133e4ce14025baa968e",
+  Description: `Lookup info on token or session guid`,
+  Subcommands: []cli.Command{
+    {
+      Name:  "application",
+      Usage: "Lookup information on an application",
+      Flags: []cli.Flag {
+        cli.BoolFlag{"addresses, a", "Include all mapped addresses"},
+        cli.BoolFlag{"pin, p", "Include mapped pins"},
+        cli.BoolFlag{"tokens, t", "Include mapped tokens"},
+        cli.BoolFlag{"numbers, n", "Include mapped numbers"},
+      },
+      Action: LookupApplication,
+    },
+    {
+      Name:  "address",
+      Usage: "Lookup information on an address",
+      Action: LookupAddress,
+    },
+    {
+      Name:  "user",
+      Usage: "Lookup a user data",
+      Flags: []cli.Flag {
+        cli.BoolFlag{"applications, a", "Include users applications"},
+      },
+      Action: LookupUser,
+    },
+  },
+}
+
+var guid = cli.Command{
   Name:  "guid",
   Usage: "tropo guid 9fb9f0887171a133e4ce14025baa968e",
   Description: `Decode runtime IP address from session guid`,
--- papi.go
+++ papi.go
@@ -1,74 +1,96 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
-  "io/ioutil"
-  "strings"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
-func provisioningApiRequest(u, p, url string) ( []byte, error ) {
-  client := &http.Client{}
-  req, err := http.NewRequest("GET",url, nil)
-  req.SetBasicAuth(u, p)
+func provisioningApiRequest(u, p, url string) ([]byte, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.SetBasicAuth(u, p)
 
-  resp, err := client.Do(req)
-  if err != nil{
-    logger.Fatal(err)
-  }
+	resp, err := client.Do(req)
+	if err != nil {
+		logger.Fatal(err)
+	}
 
-  bodyText, err := ioutil.ReadAll(resp.Body)
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	clean_response := strings.Replace(string(strings.Replace(string(bodyText), " ", "", -1)), "\n", "", -1)
+	logger.Debug("URL: %s || Response: %s [ code: %d ]", url, clean_response, resp.StatusCode)
 
-  if err != nil {
-    panic(err.Error())
-  }
-  return bodyText, err
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if resp.StatusCode == 404 {
+		logger.Fatal("Not found")
+	}
+
+	return bodyText, err
 }
 
 func GetAppData(username, password, url, application string) (string, PapiApplicationResponse) {
-  fullApiUrl := []string{url, "/applications/", application};
-  bodyText, err := provisioningApiRequest(username, password, strings.Join(fullApiUrl, ""))
+	fullApiUrl := []string{url, "/applications/", application}
+	bodyText, err := provisioningApiRequest(username, password, strings.Join(fullApiUrl, ""))
 
-  var data PapiApplicationResponse
-  err = json.Unmarshal(bodyText, &data)
+	var data PapiApplicationResponse
+	err = json.Unmarshal(bodyText, &data)
 
-  if err != nil {
-    panic(err.Error())
-  }
+	if err != nil {
+		panic(err.Error())
+	}
 
-  str := string(bodyText)
-  return str, data
+	str := string(bodyText)
+	return str, data
 }
 
 func GetAddressData(username, password, url, address string) (string, PapiAddressResponse) {
-  fullApiUrl := []string{url, "/addresses/number/", address};
-  bodyText, err := provisioningApiRequest(username, password, strings.Join(fullApiUrl, ""))
+	fullApiUrl := []string{url, "/addresses/number/", address}
+	bodyText, err := provisioningApiRequest(username, password, strings.Join(fullApiUrl, ""))
 
-  var data PapiAddressResponse
-  err = json.Unmarshal(bodyText, &data)
+	var data PapiAddressResponse
+	err = json.Unmarshal(bodyText, &data)
 
-  if err != nil {
-    panic(err.Error())
-  }
+	if err != nil {
+		panic(err.Error())
+	}
 
-  str := string(bodyText)
-  return str, data
+	str := string(bodyText)
+	return str, data
 }
 
 func GetUserData(username, password, apiUrl, accountName string) (string, PapiUserResponse) {
+	logger.Debug("Looking up user - %s", accountName)
+	fullApiUrl := []string{apiUrl, "/users/", string(accountName)}
+	bodyText, err := provisioningApiRequest(username, password, strings.Join(fullApiUrl, ""))
 
-  fullApiUrl := []string{apiUrl, "/users/", accountName};
-  bodyText, err := provisioningApiRequest(username, password, strings.Join(fullApiUrl, ""))
+	var data PapiUserResponse
+	err = json.Unmarshal(bodyText, &data)
 
-  var data PapiUserResponse
-  err = json.Unmarshal(bodyText, &data)
+	if err != nil {
+		panic(err.Error())
+	}
 
-  if err != nil {
-    panic(err.Error())
-  }
+	str := string(bodyText)
+	return str, data
+}
 
-  str := string(bodyText)
-  return str, data
+func GetUsersApplications(username, password, apiUrl, accountName string) Applications {
+	logger.Debug("Looking up user's applications - %s", accountName)
+	fullApiUrl := []string{apiUrl, "/users/", string(accountName), "/applications"}
+	bodyText, err := provisioningApiRequest(username, password, strings.Join(fullApiUrl, ""))
+
+	var data Applications
+	err = json.Unmarshal(bodyText, &data)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return data
 }
 
 func GetApplicationAddresses(username, password, apiUrl, application string) ApplicationAddresses {
@@ -87,23 +109,24 @@ func GetApplicationAddresses(username, password, apiUrl, application string) App
 }
 
 func GetUserFeatures(username, password, apiUrl, accountName string) []string {
+	logger.Debug("Looking up accountName - %s", accountName)
 
-  fullApiUrl := []string{apiUrl, "/users/", accountName, "/features"};
-  bodyText, err := provisioningApiRequest(username, password, strings.Join(fullApiUrl, ""))
+	fullApiUrl := []string{apiUrl, "/users/", accountName, "/features"}
+	bodyText, err := provisioningApiRequest(username, password, strings.Join(fullApiUrl, ""))
 
-  var data PapiFeaturesResponse
-  err = json.Unmarshal(bodyText, &data)
+	var data PapiFeaturesResponse
+	err = json.Unmarshal(bodyText, &data)
 
+	if err != nil {
+		panic(err.Error())
+	}
+	logger.Debug("Features list: ", data)
 
-  if err != nil {
-    panic(err.Error())
-  }
+	str := []string{}
+	for _, v := range data {
+		str = append(str, v.FeatureFlag)
+	}
 
-  str := []string{}
-  for _, v := range data {
-    str = append(str,v.FeatureFlag)
-  }
-
-  //str := string(bodyText)
-  return str
+	//str := string(bodyText)
+	return str
 }
