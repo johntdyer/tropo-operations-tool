f00c25bd989307a175095b14787c3f2098ae28ca
John Dyer <jdyer@tropo.com>
2014-07-06 21:43:27 -0400

    Move "actions" into their own file

---
+++ actions.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+  "github.com/codegangsta/cli"
+  "github.com/wsxiaoys/terminal"
+  tropo "bitbucket.org/voxeolabs/go-tropo-utils"
+)
+
+func PrintFeatures(c *cli.Context) {
+  BuildFeaturesTable()
+}
+
+
+func DecodeSessionID(c *cli.Context) {
+  session :=  c.Args().First()
+
+  CheckForRequiredArguments(session,"guid 23b1a72988afe289a11800ce553ab6b6")
+
+  s := tropo.DecodeSessionId(session)
+  terminal.Stdout.Color("y").Print("Results: ", s.IP).Nl().Reset()
+}
+
+func LookupAddress(c *cli.Context) {
+  address :=  c.Args().First()
+
+  CheckForRequiredArguments(address,"lookup address +14075551212")
+
+  str, json := GetAddressData(user, password, rest_api, address)
+  logger.Debug("Address Response: ", str)
+  terminal.Stdout.Color("y").Print("Results").Nl().Reset()
+  BuildAddressTable(json)
+}
+
+func LookupUser(c *cli.Context) {
+  account :=  c.Args().First()
+
+  CheckForRequiredArguments(account,"lookup user 1234")
+
+  features := []string{}
+  str, userData := GetUserData(user, password, rest_api, account)
+  features       = GetUserFeatures(user, password, rest_api, account)
+  var _ = str
+  terminal.Stdout.Color("y").Print("Results").Nl().Reset()
+  BuildUserTable(userData,features)
+  if c.Bool("applications") == true {
+    applications := GetUsersApplications(user, password, rest_api, account)
+    BuildApplicationsTable(applications)
+  }
+}
+
+func doLookup(c *cli.Context) {
+}
+
+func LookupApplication(c *cli.Context) {
+  application :=  c.Args().First()
+
+  CheckForRequiredArguments(application,"lookup application 1234")
+
+  str, json := GetAppData(user, password, rest_api, application)
+  var _ = str
+  terminal.Stdout.Color("y").Print("Results").Nl().Reset()
+
+  BuildApplicationTable(json)
+  if c.Bool("addresses") == true {
+    data := GetApplicationAddresses(user, password, rest_api, application)
+    BuildApplicationAddressesTable(data)
+  }
+
+
+}
--- commands.go
+++ commands.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
   "github.com/codegangsta/cli"
-  "github.com/wsxiaoys/terminal"
-  tropo "bitbucket.org/voxeolabs/go-tropo-utils"
 )
 
 var Commands = []cli.Command{
@@ -18,112 +15,18 @@ var sessionIdLookup = cli.Command{
   Name:  "guid",
   Usage: "tropo guid 9fb9f0887171a133e4ce14025baa968e",
   Description: `Decode runtime IP address from session guid`,
-  Action: doSessionLookup,
+  Action: DecodeSessionID,
 }
 
-var commandAddressLookup = cli.Command{
-  Name:  "address",
-  Usage: "tropo address --number +14074740214",
-  Description: `Lookup adddress data`,
-  Flags: []cli.Flag {
-    cli.StringFlag{"pin, p", "", "Sip pin to lookup, eg 9995551212"},
-    cli.StringFlag{"number, n", "", "Number to lookup, Must include + and country code ( +14075551212 ) "},
-    cli.StringFlag{"token, t", "", "Address to lookup."},
-  },
-  Action: doAddressLookup,
-}
-
-var commandUserLookup = cli.Command{
-  Name:  "user",
-  Usage: "tropo user --user jdyer",
+var list = cli.Command{
+  Name:  "list",
+  Usage: "tropo list features",
   Description: ``,
-  Flags: []cli.Flag {
-    cli.StringFlag{"user, u", "", "account to lookup.  Both id & username are supported"},
+  Subcommands: []cli.Command{
+    {
+      Name:  "features",
+      Usage: "Print feature flags",
+      Action: PrintFeatures,
+    },
   },
-  Action: doUserLookup,
 }
-
-var commandApplicationLookup = cli.Command{
-	Name:  "application",
-	Usage: "tropo application --app 123456",
-	Description: `Lookup application data `,
-  Flags: []cli.Flag {
-    cli.StringFlag{"app, a", "", "Application ID to lookup."},
-  },
-	Action: doApplicationLookup,
-}
-
-func assert(err error) {
-	if err != nil {
-		logger.Fatal(err)
-	}
-}
-
-func doSessionLookup(c *cli.Context) {
-  s := tropo.DecodeSessionId(c.Args()[0])
-  terminal.Stdout.Color("y").Print("Results: ", s.IP).Nl().Reset()
-}
-
-func doAddressLookup(c *cli.Context) {
-  user, password, url := GetPapiConfig()
-
-  if c.String("ff") != "" {
-  //   terminal.Stdout.Color("r").Print("-- ERROR -- Missing argument").Nl().Reset()
-  //   terminal.Stdout.Color("b").Print("  Examples: \n").Nl().Reset()
-  // else if c.String("number") == "" {
-  //   terminal.Stdout.Color("r").Print("-- ERROR -- Missing argument").Nl().Reset()
-  //   terminal.Stdout.Color("b").Print("  Examples: \n").Nl().Reset()
-  //   terminal.Stdout.Color("b").Print("    tropo address --number +14075551212").Nl().Reset()
-  //   }
-
-
-  //   terminal.Stdout.Color("b").Print("    tropo address --pin 9995551212").Nl().Reset()
-  //   terminal.Stdout.Color("b").Print("    tropo address --token cf77f59c97b9a44c950446eff97a37bc100212a34ed68a4b0115dfc79d9f3b73446b9995c9170bdec6e1a889").Nl().Reset()
-
-  //   fmt.Println("---------------")
-  //   cli.ShowAppHelp(c)
-  }else {
-    str, json := GetAddressData(user, password, url, c.String("number"))
-    logger.Debug("Address Response: ", str)
-    terminal.Stdout.Color("y").Print("Results").Nl().Reset()
-    BuildAddressTable(json)
-  }
-}
-
-func doUserLookup(c *cli.Context) {
-  user, password, url := GetPapiConfig()
-
-  if c.String("user") == "" {
-    terminal.Stdout.Color("r").Print("-- ERROR -- Missing argument").Nl().Reset()
-    terminal.Stdout.Color("b").Print("  Example: \n    tropo user --user 1234").Nl().Reset()
-    fmt.Println("---------------")
-    cli.ShowAppHelp(c)
-  }else {
-
-    features := []string{}
-
-    str, userData := GetUserData(user, password, url, c.String("user"))
-    features = GetUserFeatures(user, password, url, c.String("user"))
-    var _ = str
-    terminal.Stdout.Color("y").Print("Results").Nl().Reset()
-    BuildUserTable(userData,features)
-  }
-}
-
-func doApplicationLookup(c *cli.Context) {
-  user, password, url := GetPapiConfig()
-
-  if c.String("app") == "" {
-    terminal.Stdout.Color("r").Print("-- ERROR -- Missing argument").Nl().Reset()
-    terminal.Stdout.Color("b").Print("  Example: \n    tropo user --user 1234").Nl().Reset()
-    fmt.Println("---------------")
-    cli.ShowAppHelp(c)
-  }else {
-    str, json := GetAppData(user, password, url, c.String("app"))
-    var _ = str
-    terminal.Stdout.Color("y").Print("Results").Nl().Reset()
-    BuildApplicationTable(json)
-  }
-
-}
-
