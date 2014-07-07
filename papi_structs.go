d90f8c7d34477247a9442ad6c9646985eca420a2
John Dyer <jdyer@tropo.com>
2014-07-06 21:46:10 -0400

    indentation fixes

--- papi_structs.go
+++ papi_structs.go
@@ -11,49 +11,49 @@ type Config struct {
 }
 
 type PapiApplicationResponse struct {
-    Id                      string
-    Name                    string
-    Platform                string
-    VoiceEnvironmentId      string
-    MessagingEnvironmentId  string
-    MessagingUrl            string
-    VoiceUrl                string
-    Environment             string
-    UserId                  int
-    Partition               string
+  Id                      string
+  Name                    string
+  Platform                string
+  VoiceEnvironmentId      string
+  MessagingEnvironmentId  string
+  MessagingUrl            string
+  VoiceUrl                string
+  Environment             string
+  UserId                  int
+  Partition               string
 }
 
 type PapiUserResponse struct {
-    Id   string
-    Address     string
-    Address2    string
-    Username    string
-    State       string
-    Email       string
-    FirstName   string
-    LastName    string
-    Notes       string
-    Status      string
-    JoinDate    string
-    PasswordFailedAttempts int
+  Id          string
+  Address     string
+  Address2    string
+  Username    string
+  State       string
+  Email       string
+  FirstName   string
+  LastName    string
+  Notes       string
+  Status      string
+  JoinDate    string
+  PasswordFailedAttempts int
 }
 
 type PapiAddressResponse struct {
-    Type                string
-    Prefix              string
-    Number              string
-    DisplayNumber       string
-    ServiceId           string
-    City                string
-    State               string
-    Country             string
-    ProviderName        string
-    SmsEnabled          bool
-    ExcludeFromBilling  bool
-    SmsRateLimit        int
-    ExchangeId          int
-    ApplicationId       int
-    RequireVerification bool
+  Type                string
+  Prefix              string
+  Number              string
+  DisplayNumber       string
+  ServiceId           string
+  City                string
+  State               string
+  Country             string
+  ProviderName        string
+  SmsEnabled          bool
+  ExcludeFromBilling  bool
+  SmsRateLimit        int
+  ExchangeId          int
+  ApplicationId       int
+  RequireVerification bool
 }
 
 type PapiFeaturesResponse []struct  {
