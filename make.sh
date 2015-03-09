1138e998d740d68e395c73d13245aa8fb8b266bd
John Dyer <jdyer@tropo.com>
2015-03-09 09:35:37 -0400

    Rename to TOT - Tropo Operations Tool

--- make.sh
+++ make.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 CGO_ENABLED=0
 
+echo "Building Tropo Operations Tool"
 # Build for OSX
-go build -o builds/tropo.osx tropo.go papi.go papi_structs.go utils.go config.go commands.go actions.go address_helper.go sip.go tables.go version.go
+go build -o builds/tot.osx tot.go papi.go papi_structs.go utils.go config.go commands.go actions.go address_helper.go sip.go tables.go version.go
 if [ $? -eq 0 ]; then
-  echo "Success Build artifact - builds/tropo.osx"
+  echo "Success Build artifact - builds/tot.osx"
 else
   echo "Build error"
   exit $?
@@ -14,9 +15,9 @@ fi
 GOOS=linux
 GOARCH=amd64
 CGO_ENABLED=0
-go build -o builds/tropo.linux tropo.go papi.go papi_structs.go utils.go config.go commands.go actions.go address_helper.go sip.go tables.go version.go
+go build -o builds/tot.linux tot.go papi.go papi_structs.go utils.go config.go commands.go actions.go address_helper.go sip.go tables.go version.go
 if [ $? -eq 0 ]; then
-  echo "Success Build artifact - builds/tropo.linux"
+  echo "Success Build artifact - builds/tot.linux"
 else
   echo "Build error"
   exit $?
