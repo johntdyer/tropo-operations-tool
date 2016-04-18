153c4be80a2cd5acea9fdac4aa15ae2e699919b2
John Dyer <johndye@cisco.com>
2016-04-18 10:20:46 -0400

    go lint fixes for address helper

--- address_helper.go
+++ address_helper.go
@@ -1,31 +1,32 @@
 package main
 
-import(
-  "strings"
-  "github.com/op/go-logging"
-  //tropo "bitbucket.org/voxeolabs/go-tropo-utils"
+import (
+	"strings"
+
+	"github.com/op/go-logging"
+	//tropo "bitbucket.org/voxeolabs/go-tropo-utils"
 )
 
-func testinf(){
-  logging.SetLevel(logging.ERROR, "test")
-  AddressCleaner("cf77f59c97b9a44c950446eff97a37bc100212a34ed68a4b0115dfc79d9f3b73446b9995c9170bdec6e1a889")
-  AddressCleaner("9995551212@sip.tropo.com")
-  AddressCleaner("tel:+14074740214")
-  //tropo.encodeToken("cf77f59c97b9a44c950446eff97a37bc100212a34ed68a4b0115dfc79d9f3b73446b9995c9170bdec6e1a889")
+func testinf() {
+	logging.SetLevel(logging.ERROR, "test")
+	addressCleaner("cf77f59c97b9a44c950446eff97a37bc100212a34ed68a4b0115dfc79d9f3b73446b9995c9170bdec6e1a889")
+	addressCleaner("9995551212@sip.tropo.com")
+	addressCleaner("tel:+14074740214")
+	//tropo.encodeToken("cf77f59c97b9a44c950446eff97a37bc100212a34ed68a4b0115dfc79d9f3b73446b9995c9170bdec6e1a889")
 }
 
-func AddressCleaner(address string) string {
-  addr := strings.TrimPrefix(strings.Split(address,"@")[0],"tel:")
-  logger.Debug("Sanitized address: %s", addr)
-  return addr
+func addressCleaner(address string) string {
+	addr := strings.TrimPrefix(strings.Split(address, "@")[0], "tel:")
+	logger.Debug("Sanitized address: %s", addr)
+	return addr
 }
 
-func AddressType(address string) string {
-  if len(address) == 88 {
-    return "token"
-  }else if strings.HasPrefix(address, "999"){
-    return "pin"
-  }else{
-    return "number"
-  }
+func addressType(address string) string {
+	if len(address) == 88 {
+		return "token"
+	} else if strings.HasPrefix(address, "999") {
+		return "pin"
+	} else {
+		return "number"
+	}
 }
