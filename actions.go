db7df12c58ac12266d85ac693e7f3fb9477eeae9
John Dyer <johndye@cisco.com>
2016-04-18 10:19:44 -0400

    Refactor actions file to use kingpin.v2

--- actions.go
+++ actions.go
@@ -1,88 +1,125 @@
 package main
 
 import (
-	tropo "bitbucket.org/voxeolabs/go-tropo-utils"
+	"strconv"
+
+	"bitbucket.org/voxeolabs/go-tropo-utils"
 	"github.com/codegangsta/cli"
 	"github.com/wsxiaoys/terminal"
 )
 
-func PrintFeatures(c *cli.Context) {
-	BuildFeaturesTable()
-}
-
-func PrintPpids(c *cli.Context) {
-	BuildPpidsTable()
-}
-
-func PrintAllCallBacks(c *cli.Context) {
-	BuildSipTable("", true, false)
-}
-
-func DecodeSessionID(c *cli.Context) {
-	session := c.Args().First()
-
-	CheckForRequiredArguments(session, "guid 23b1a72988afe289a11800ce553ab6b6")
-
-	s := tropo.DecodeSessionID(session)
-	terminal.Stdout.Color("y").Print("Results: ", s.IP).Nl().Reset()
-}
-
-func LookupSipCode(c *cli.Context) {
-	code := c.Args().First()
-	var showAll bool
-	if c.Bool("all") == true {
-		showAll = true
-	} else {
-		showAll = false
-	}
-
-	BuildSipTable(code, showAll, false)
-}
-
-func LookupAddress(c *cli.Context) {
-	address := c.Args().First()
-
-	CheckForRequiredArguments(address, "lookup address +14075551212")
-
-	str, json := GetAddressData(user, password, rest_api, address)
+// addressLookupAction
+func addressLookupAction(address string) {
+	str, addressData := getAddressData(address)
 	logger.Debug("Address Response: ", str)
 	terminal.Stdout.Color("y").Print("Results").Nl().Reset()
-	BuildAddressTable(json)
+	buildAddressTable(addressData)
+
+	if addressData.ApplicationID != 0 {
+		_, appData := getAppData(strconv.Itoa(addressData.ApplicationID))
+		features := getUserFeatures(strconv.Itoa(appData.UserID))
+		_, userData := getUserData(strconv.Itoa(appData.UserID))
+
+		buildApplicationTable(appData)
+		buildUserTable(userData, features)
+	}
 }
 
-func LookupUser(c *cli.Context) {
-	account := c.Args().First()
+// sipCodeLookupAction
+func sipCodeLookupAction(code string) {
+	var showAllSIPCodes bool
+	if *sipCode == "" {
+		showAllSIPCodes = true
+	} else {
+		showAllSIPCodes = false
+	}
+	buildSipTable(*sipCode, showAllSIPCodes, *includeRFCS)
+}
 
-	CheckForRequiredArguments(account, "lookup user 1234")
+// decodeSessionIDAction - Decode a sessionGUID
+func decodeSessionIDAction(sessionID string) {
 
+	s := tropo.DecodeSessionID(sessionID)
+	terminal.Stdout.Color("y").Print(sessionID + " -> ").Reset()
+	terminal.Stdout.Color("r").Print(s.IP).Nl().Reset()
+}
+
+// userLookupAction
+func userLookupAction(user string) {
 	features := []string{}
-	str, userData := GetUserData(user, password, rest_api, account)
-	features = GetUserFeatures(user, password, rest_api, account)
-	var _ = str
+	_, userData := getUserData(user)
+	features = getUserFeatures(user)
+
 	terminal.Stdout.Color("y").Print("Results").Nl().Reset()
-	BuildUserTable(userData, features)
-	if c.Bool("applications") == true {
-		applications := GetUsersApplications(user, password, rest_api, account)
-		BuildApplicationsTable(applications)
+	buildUserTable(userData, features)
+
+	if *includeApplications {
+		applications := getUsersApplications(user)
+		buildApplicationsTable(applications)
 	}
 }
 
-func doLookup(c *cli.Context) {
-}
-
-func LookupApplication(c *cli.Context) {
-	application := c.Args().First()
-
-	CheckForRequiredArguments(application, "lookup application 1234")
-
-	str, json := GetAppData(user, password, rest_api, application)
-	var _ = str
+// applicationLookupAction
+func applicationLookupAction(applicationID string) {
+	_, json := getAppData(applicationID)
 	terminal.Stdout.Color("y").Print("Results").Nl().Reset()
 
-	BuildApplicationTable(json)
-	if c.Bool("addresses") == true {
-		data := GetApplicationAddresses(user, password, rest_api, application)
-		BuildApplicationAddressesTable(data)
+	buildApplicationTable(json)
+	if *includeAddresses == true {
+		data := getApplicationAddresses(applicationID)
+		buildApplicationAddressesTable(data)
 	}
 
 }
+
+func manageFeature(c *cli.Context) {
+	if c.Args().Present() {
+		userName := c.Args().First()
+		if c.Bool("list") {
+			buildFeaturesTable()
+		}
+
+		if true { //.Bool("show") {
+			// fmt.Println(restAPI)
+			getUserFeatures(userName)
+
+		}
+	}
+}
+
+// 	// if c.Bool("disable") {
+//
+// 	// 	str, err := provisioningApiPost(user, password, "https://api.tropo.com/v1/users/"+userName+"/features", []byte(`{"feature":"10","featureFlag":"x"}`))
+//
+// 	// 	if err != nil {
+// 	// 		fmt.Println(err)
+// 	// 	}
+// 	// 	fmt.Println(str)
+// 	// }
+//
+// 	// if c.Bool("enable") {
+// 	// 	fmt.Println("Enabling")
+// 	// 	str, err := provisioningApiDelete(user, password, "https://api.tropo.com/v1/users/"+userName+"/features/10")
+// 	// 	if err != nil {
+// 	// 		fmt.Println(err)
+// 	// 	}
+// 	// 	fmt.Println("Enabled")
+// 	// 	fmt.Println(str)
+//
+// 	// }
+// 	//  map[string]int{
+// 	// 	"c": 1,
+// 	// 	"u": 2,
+// 	// 	"i": 3,
+// 	// 	"s": 4,
+// 	// 	"r": 5,
+// 	// 	"b": 6,
+// 	//  "t": 7,
+// 	// 	"d": 8,
+// 	// 	"w": 9,
+// 	//  "x" : 10,
+// 	// }
+// 	fmt.Println(c.StringSlice("add"))
+// 	fmt.Println(c.StringSlice("remove"))
+//
+// }
