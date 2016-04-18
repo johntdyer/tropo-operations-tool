bb4d70a69d942819e3ac5f63bb362646765de3f3
John Dyer <johndye@cisco.com>
2016-04-18 10:15:36 -0400

    Go lint fixes and also handle table w/ and w/o rfcs

--- sip.go
+++ sip.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"github.com/olekukonko/tablewriter"
-	"github.com/wsxiaoys/terminal"
 	"os"
 	"sort"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/wsxiaoys/terminal"
 	// "strconv"
 )
 
-type SipMessage struct {
+type sipMessage struct {
 	Reason       string
 	Rfc          string
 	TropoHandler string
@@ -20,360 +21,360 @@ type SipMessage struct {
 // 	BuildSipTable("486", true)
 // }
 
-func BuildSipTable(code string, showAll, includeRfcs bool) {
-	var sipMessages = make(map[string]*SipMessage, 100)
+func buildSipTable(code string, showAll, includeRfcs bool) {
+	var sipMessages = make(map[string]*sipMessage, 200)
 
-	sipMessages["100"] = &SipMessage{
+	sipMessages["100"] = &sipMessage{
 		Reason:       "Trying",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["180"] = &SipMessage{
+	sipMessages["180"] = &sipMessage{
 		Reason:       "Ringing",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["181"] = &SipMessage{
+	sipMessages["181"] = &sipMessage{
 		Reason:       "Call Is Being Forwarded",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["182"] = &SipMessage{
+	sipMessages["182"] = &sipMessage{
 		Reason:       "Queued",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["183"] = &SipMessage{
+	sipMessages["183"] = &sipMessage{
 		Reason:       "Session Progress",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["199"] = &SipMessage{
+	sipMessages["199"] = &sipMessage{
 		Reason:       "Early Dialog Terminated",
 		Rfc:          "RFC6228",
 		TropoHandler: "n/a",
 	}
-	sipMessages["200"] = &SipMessage{
+	sipMessages["200"] = &sipMessage{
 		Reason:       "OK",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["202"] = &SipMessage{
+	sipMessages["202"] = &sipMessage{
 		Reason:       "Accepted (Deprecated)",
 		Rfc:          "RFC6665",
 		TropoHandler: "n/a",
 	}
-	sipMessages["204"] = &SipMessage{
+	sipMessages["204"] = &sipMessage{
 		Reason:       "No Notification",
 		Rfc:          "RFC5839",
 		TropoHandler: "n/a",
 	}
-	sipMessages["300"] = &SipMessage{
+	sipMessages["300"] = &sipMessage{
 		Reason:       "Multiple Choices",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["301"] = &SipMessage{
+	sipMessages["301"] = &sipMessage{
 		Reason:       "Moved Permanently",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["302"] = &SipMessage{
+	sipMessages["302"] = &sipMessage{
 		Reason:       "Moved Temporarily",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["305"] = &SipMessage{
+	sipMessages["305"] = &sipMessage{
 		Reason:       "Use Proxy",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["380"] = &SipMessage{
+	sipMessages["380"] = &sipMessage{
 		Reason:       "Alternative Service",
 		Rfc:          "RFC3261",
 		TropoHandler: "n/a",
 	}
-	sipMessages["400"] = &SipMessage{
+	sipMessages["400"] = &sipMessage{
 		Reason:       "Bad Request",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["401"] = &SipMessage{
+	sipMessages["401"] = &sipMessage{
 		Reason:       "Unauthorized",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["402"] = &SipMessage{
+	sipMessages["402"] = &sipMessage{
 		Reason:       "Payment Required",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["403"] = &SipMessage{
+	sipMessages["403"] = &sipMessage{
 		Reason:       "Forbidden",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["404"] = &SipMessage{
+	sipMessages["404"] = &sipMessage{
 		Reason:       "Not Found",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["405"] = &SipMessage{
+	sipMessages["405"] = &sipMessage{
 		Reason:       "Method Not Allowed",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["406"] = &SipMessage{
+	sipMessages["406"] = &sipMessage{
 		Reason:       "Not Acceptable",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["407"] = &SipMessage{
+	sipMessages["407"] = &sipMessage{
 		Reason:       "Proxy Authentication Required",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["408"] = &SipMessage{
+	sipMessages["408"] = &sipMessage{
 		Reason:       "Request Timeout",
 		Rfc:          "RFC3261",
 		TropoHandler: "onTimeout",
 	}
-	sipMessages["410"] = &SipMessage{
+	sipMessages["410"] = &sipMessage{
 		Reason:       "Gone",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["412"] = &SipMessage{
+	sipMessages["412"] = &sipMessage{
 		Reason:       "Conditional Request Failed",
 		Rfc:          "RFC3903",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["413"] = &SipMessage{
+	sipMessages["413"] = &sipMessage{
 		Reason:       "Request Entity Too Large",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["414"] = &SipMessage{
+	sipMessages["414"] = &sipMessage{
 		Reason:       "Request-URI Too Long",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["415"] = &SipMessage{
+	sipMessages["415"] = &sipMessage{
 		Reason:       "Unsupported Media Type",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["416"] = &SipMessage{
+	sipMessages["416"] = &sipMessage{
 		Reason:       "Unsupported URI Scheme",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["417"] = &SipMessage{
+	sipMessages["417"] = &sipMessage{
 		Reason:       "Unknown Resource-Priority",
 		Rfc:          "RFC4412",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["420"] = &SipMessage{
+	sipMessages["420"] = &sipMessage{
 		Reason:       "Bad Extension",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["421"] = &SipMessage{
+	sipMessages["421"] = &sipMessage{
 		Reason:       "Extension Required",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["422"] = &SipMessage{
+	sipMessages["422"] = &sipMessage{
 		Reason:       "Session Interval Too Small",
 		Rfc:          "RFC4028",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["423"] = &SipMessage{
+	sipMessages["423"] = &sipMessage{
 		Reason:       "Interval Too Brief",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["424"] = &SipMessage{
+	sipMessages["424"] = &sipMessage{
 		Reason:       "Bad Location Information",
 		Rfc:          "RFC6442",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["428"] = &SipMessage{
+	sipMessages["428"] = &sipMessage{
 		Reason:       "Use Identity Header",
 		Rfc:          "RFC4474",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["429"] = &SipMessage{
+	sipMessages["429"] = &sipMessage{
 		Reason:       "Provide Referrer Identity",
 		Rfc:          "RFC3892",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["430"] = &SipMessage{
+	sipMessages["430"] = &sipMessage{
 		Reason:       "Flow Failed",
 		Rfc:          "RFC5626",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["433"] = &SipMessage{
+	sipMessages["433"] = &sipMessage{
 		Reason:       "Anonymity Disallowed",
 		Rfc:          "RFC5079",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["436"] = &SipMessage{
+	sipMessages["436"] = &sipMessage{
 		Reason:       "Bad Identity-Info",
 		Rfc:          "RFC4474",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["437"] = &SipMessage{
+	sipMessages["437"] = &sipMessage{
 		Reason:       "Unsupported Certificate",
 		Rfc:          "RFC4474",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["438"] = &SipMessage{
+	sipMessages["438"] = &sipMessage{
 		Reason:       "Invalid Identity Header",
 		Rfc:          "RFC4474",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["439"] = &SipMessage{
+	sipMessages["439"] = &sipMessage{
 		Reason:       "First Hop Lacks Outbound Support",
 		Rfc:          "RFC5626",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["440"] = &SipMessage{
+	sipMessages["440"] = &sipMessage{
 		Reason:       "Max-Breadth Exceeded",
 		Rfc:          "RFC5393",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["469"] = &SipMessage{
+	sipMessages["469"] = &sipMessage{
 		Reason:       "Bad Info Package",
 		Rfc:          "RFC6086",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["470"] = &SipMessage{
+	sipMessages["470"] = &sipMessage{
 		Reason:       "Consent Needed",
 		Rfc:          "RFC5360",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["480"] = &SipMessage{
+	sipMessages["480"] = &sipMessage{
 		Reason:       "Temporarily Unavailable",
 		Rfc:          "RFC3261",
 		TropoHandler: "onBusy",
 	}
-	sipMessages["481"] = &SipMessage{
+	sipMessages["481"] = &sipMessage{
 		Reason:       "Call/Transaction Does Not Exist",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["482"] = &SipMessage{
+	sipMessages["482"] = &sipMessage{
 		Reason:       "Loop Detected",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["483"] = &SipMessage{
+	sipMessages["483"] = &sipMessage{
 		Reason:       "Too Many Hops",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["484"] = &SipMessage{
+	sipMessages["484"] = &sipMessage{
 		Reason:       "Address Incomplete",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["485"] = &SipMessage{
+	sipMessages["485"] = &sipMessage{
 		Reason:       "Ambiguous",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["486"] = &SipMessage{
+	sipMessages["486"] = &sipMessage{
 		Reason:       "Busy Here",
 		Rfc:          "RFC3261",
 		TropoHandler: "onBusy",
 	}
-	sipMessages["487"] = &SipMessage{
+	sipMessages["487"] = &sipMessage{
 		Reason:       "Request Terminated",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["488"] = &SipMessage{
+	sipMessages["488"] = &sipMessage{
 		Reason:       "Not Acceptable Here",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["489"] = &SipMessage{
+	sipMessages["489"] = &sipMessage{
 		Reason:       "Bad Event",
 		Rfc:          "RFC6665",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["491"] = &SipMessage{
+	sipMessages["491"] = &sipMessage{
 		Reason:       "Request Pending",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["493"] = &SipMessage{
+	sipMessages["493"] = &sipMessage{
 		Reason:       "Undecipherable",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["494"] = &SipMessage{
+	sipMessages["494"] = &sipMessage{
 		Reason:       "Security Agreement Required",
 		Rfc:          "RFC3329",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["500"] = &SipMessage{
+	sipMessages["500"] = &sipMessage{
 		Reason:       "Server Internal Error",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["501"] = &SipMessage{
+	sipMessages["501"] = &sipMessage{
 		Reason:       "Not Implemented",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["502"] = &SipMessage{
+	sipMessages["502"] = &sipMessage{
 		Reason:       "Bad Gateway",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["503"] = &SipMessage{
+	sipMessages["503"] = &sipMessage{
 		Reason:       "Service Unavailable",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["504"] = &SipMessage{
+	sipMessages["504"] = &sipMessage{
 		Reason:       "Server Time-out",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["505"] = &SipMessage{
+	sipMessages["505"] = &sipMessage{
 		Reason:       "Version Not Supported",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["513"] = &SipMessage{
+	sipMessages["513"] = &sipMessage{
 		Reason:       "Message Too Large",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["580"] = &SipMessage{
+	sipMessages["580"] = &sipMessage{
 		Reason:       "Precondition Failure",
 		Rfc:          "RFC3312",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["600"] = &SipMessage{
+	sipMessages["600"] = &sipMessage{
 		Reason:       "Busy Everywhere",
 		Rfc:          "RFC3261",
 		TropoHandler: "onBusy",
 	}
-	sipMessages["603"] = &SipMessage{
+	sipMessages["603"] = &sipMessage{
 		Reason:       "Decline",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["604"] = &SipMessage{
+	sipMessages["604"] = &sipMessage{
 		Reason:       "Does Not Exist Anywhere",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
 	}
-	sipMessages["606"] = &SipMessage{
+	sipMessages["606"] = &sipMessage{
 		Reason:       "Not Acceptable",
 		Rfc:          "RFC3261",
 		TropoHandler: "onCallFailure",
@@ -416,9 +417,15 @@ func BuildSipTable(code string, showAll, includeRfcs bool) {
 
 		table.Render()
 	} else {
+
 		s, ok := sipMessages[code]
 		if ok {
-			table.Append([]string{code, s.Reason, s.TropoHandler, s.Rfc})
+			if includeRfcs {
+				table.Append([]string{code, s.Reason, s.TropoHandler, s.Rfc})
+			} else {
+				table.Append([]string{code, s.Reason, s.TropoHandler})
+			}
+
 			table.Render()
 		} else {
 			terminal.Stdout.Color("r").Print("Invalid or unknown sip code").Nl().Reset()
